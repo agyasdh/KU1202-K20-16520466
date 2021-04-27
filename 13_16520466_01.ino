b405f97aceade20d580c462f1dc3de76722ba6d5
agyasdh <81350577+agyasdh@users.noreply.github.com>
2021-04-27 15:18:57 +0700

    Kode Exercise 1 Minggu 13

--- 13_16520466_01.ino
+++ 13_16520466_01.ino
@@ -1,9 +1,10 @@
+#include <LiquidCrystal.h>
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 void setup()
 {
   lcd.begin(16, 2);
-  lcd.print("hello, world!")
+  lcd.print("hello, world!");
 }
 
 void loop()
